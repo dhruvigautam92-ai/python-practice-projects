9bdfc734909c26cf366d5a9a993398c771b3f7dd
dhruvigautam92-ai <dhruvigautam92@gmail.com>
2026-02-07 10:43:11 +0530

    Add examples of list operations in Python
    Demonstrate various list operations including slicing, appending, sorting, reversing, inserting, removing, and popping elements.

---
+++ core_concepts/lists.py
@@ -0,0 +1,49 @@
+marks=[98,90.3,67.8,89,87]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(len(marks)
+      
+#operation on lists
+      
+#list slicing;cuts list 
+marks=[56,89,90,87,34]
+print(marks[2:4])
+
+#list.append;adds one element at the end
+list=[4,8,9,]
+list.append(10)
+print(list)
+
+#list.sort;shorts in ascending order
+list=[8,7,6,0]
+list.sort()
+print(list)
+
+#list.sort(reverse=true);short in descendig order
+list=[8,6,4,5]
+list.sort(reverse=True)
+print(list)
+
+#list.reverse;reverses list
+list=[7,8,6,9]
+list.reverse()
+print(list)
+
+#list.insert(idx.el);insert element at index
+list=[4,5,6]
+list.insert(1,8)
+print(list)
+
+#list.remove;remove first occurenece of element
+list=[7,5,9,7]
+list.remove(7)
+print(list)
+
+#list.pop(idx);removes element at idx
+list=[9,8,5,4]
+list.pop(2)
+print(list)
+
+
+
